c7b29ce1c7da8a14f460fabf79226660f901c5dc
Nick Hasser <nick.hasser@gmail.com>
2021-01-12 20:17:15 +0300

    added register routes

--- todo/auth.py
+++ todo/auth.py
@@ -8,3 +8,37 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET'))
+def register():
+    return render_template('auth/register.html')
+
+
+@bp.route('/register', method=('POST'))
+def register_post():
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
+
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+    elif db.execute(
+        'SELECT id FROM user WHERE username = ?', (username,)
+    ).fetchone() is not None:
+        error 'User {} is already registered.'.format(username)
+
+    if error is None:
+        db.execute(
+            'INSERT INTO user (username, password) VALUES (?, ?)',
+            (username, generate_password_hash(password))
+        )
+        db.commit()
+        return redirect(url_for('auth.login'))
+
+    flash(error)
+
+
