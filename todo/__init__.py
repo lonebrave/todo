a1baecf6f4c2e49176909844c12f6bec53b777cf
Nick Hasser <nick.hasser@gmail.com>
2021-01-12 19:54:23 +0300

    fixing syntax error

--- todo/__init__.py
+++ todo/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     # create and confiugure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='development'
+        SECRET_KEY='development',
         DATABASE=os.path.join(app.instance_path, 'todo.sqlite'),
     )
 
